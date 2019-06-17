7575cc65d9f35c3e2e2a98dd0e417204f4129f65
Skufler <matviei.skufin@gmail.com>
2019-06-17 20:54:05 +0300

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/super_calculator.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/super_calculator.dir/main.cpp.obj"
+  "super_calculator.pdb"
+  "super_calculator.exe"
+  "super_calculator.exe.manifest"
+  "libsuper_calculator.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/super_calculator.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
